e0d0e086a327c827bdac54a08d1b978eb2dc76c9
laxmiprasannaimmadi <laxmiprasanna.immadi@gmail.com>
2024-08-19 13:35:56 +0100

    adding schemas for column names

--- file_converter.py
+++ file_converter.py
@@ -7,15 +7,22 @@
 import pandas as pd
 import glob
 import json
+import re
 
 def file_converter():
     # load schemas for csv files headers
-    schemas = json.load()
+    with open('schemas.json') as headers:
+        schemas = json.load(headers)
     
     # list all CSV files in data directory
-    files = glob.glob('data/retail_db/*/part-*')
-    for file_name in files:
-        df = pd.read_csv(file_name,header=None)
-        print(df)
+    files = glob.glob('data/retail_db/*/part*')
+
+    for file in files:
+        file_details = re.split('/',file)
+        ds_names = file_details[2]
+        column_details = schemas[ds_names]
+        print(column_details)
+        df = pd.read_csv(file,header=ds_names)
+        #print(df)
 
 file_converter()
