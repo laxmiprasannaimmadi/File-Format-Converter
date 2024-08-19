0b45efc05bb801c5b5b4f0088514d947d3974dae
laxmiprasannaimmadi <laxmiprasanna.immadi@gmail.com>
2024-08-19 13:57:04 +0100

    csv to pandas dataframe converted

--- file_converter.py
+++ file_converter.py
@@ -10,6 +10,7 @@ import json
 import re
 
 def file_converter():
+    columns = []
     # load schemas for csv files headers
     with open('schemas.json') as headers:
         schemas = json.load(headers)
@@ -21,8 +22,10 @@ def file_converter():
         file_details = re.split('/',file)
         ds_names = file_details[2]
         column_details = schemas[ds_names]
-        print(column_details)
-        df = pd.read_csv(file,header=ds_names)
-        #print(df)
+        for i in range(len(column_details)):
+                columns.append(column_details[i]['column_name'])
+        print(columns)
+        df = pd.read_csv(file,names=columns)
+        print(df)
 
 file_converter()
